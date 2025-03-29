98e09237bfec9da0f5c5ec1d2d93071c7decc036
Troy Dai <git@troydai.com>
2025-03-29 09:52:42 -0700

    Refactor logging to use slog and remove unused dependencies
    - Replaced zap logger with slog for improved logging.
    - Removed unused dependencies from go.mod and go.sum.

--- go.mod
+++ go.mod
@@ -1,7 +1,3 @@
 module github.com/troydai/http-beacon
 
 go 1.24.0
-
-require go.uber.org/zap v1.27.0
-
-require go.uber.org/multierr v1.11.0 // indirect
--- main.go
+++ main.go
@@ -2,33 +2,31 @@ package main
 
 import (
 	"context"
-	"log"
+	"log/slog"
 	"net"
 	"net/http"
 	"os"
 	"os/signal"
 	"path"
 	"time"
-
-	"go.uber.org/zap"
 )
 
 func main() {
-	logger, err := zap.NewDevelopment()
-	if err != nil {
-		log.Fatalf("error create logger: %v", err)
-	}
+	logger := slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{
+		Level: slog.LevelInfo,
+	}))
 
 	lc := &net.ListenConfig{KeepAlive: -1}
 
 	lis, err := lc.Listen(context.Background(), "tcp", "127.0.0.1:8443")
 	if err != nil {
-		logger.Fatal("error start TCP listener", zap.Error(err))
+		logger.Error("error start TCP listener", "error", err)
+		os.Exit(1)
 	}
 
 	server := &http.Server{
 		Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			logger.Info("request received.", zap.String("method", r.Method), zap.String("path", r.URL.Path))
+			logger.Info("request received", "method", r.Method, "path", r.URL.Path)
 			w.Header().Set("Content-Type", "text/plain")
 			w.Header().Set("Server-Name", "troydai/http-beacon")
 			w.WriteHeader(http.StatusOK)
@@ -41,13 +39,13 @@ func main() {
 
 	go func() {
 		defer close(chServerStopped)
-		logger.Info("server started.")
+		logger.Info("server started")
 		err := server.ServeTLS(
 			lis,
 			path.Join(os.Getenv("PWD"), "certs/cert.pem"),
 			path.Join(os.Getenv("PWD"), "certs/key.pem"),
 		)
-		logger.Info("server stopped. an error is always returned.", zap.Error(err))
+		logger.Info("server stopped. an error is always returned", "error", err)
 	}()
 
 	go func() {
@@ -57,10 +55,10 @@ func main() {
 			ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 			defer cancel()
 
-			logger.Info("shutting down server. wait for at most 10 seconds.")
+			logger.Info("shutting down server. wait for at most 10 seconds")
 
 			err := server.Shutdown(ctx)
-			logger.Info("server closed. an error may be returned", zap.Error(err))
+			logger.Info("server closed. an error may be returned", "error", err)
 		}
 		chExit <- 0
 		close(chExit)
@@ -68,10 +66,10 @@ func main() {
 
 	select {
 	case <-chServerStopped:
-		logger.Info("server stopped. exiting.")
+		logger.Info("server stopped. exiting")
 		os.Exit(0)
 	case code := <-chExit:
-		logger.Info("exit signal received. exiting.")
+		logger.Info("exit signal received. exiting")
 		os.Exit(code)
 	}
 }
